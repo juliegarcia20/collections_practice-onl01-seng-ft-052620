8857031e0135a59bcbe8884d16303641a5fa225f
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-09 02:10:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
-
+def sort_array_asc(array)
+ array.sort do |a, b|
+  a <=> b
+ end
+end
+def sort_array_desc(array)
+ array.sort! {|a, b| b <=> a}
+end
+def sort_array_char_count(array)
+   array.sort_by(&:length)
+end
+def swap_elements(array)
+  
